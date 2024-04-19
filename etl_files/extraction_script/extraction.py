02368c3afd336dc483c41b8b5171e0ea37d1d424
Revson Vieira <55165647+RevsonV@users.noreply.github.com>
2024-04-19 16:45:12 -0300

    New folders structure and code

--- etl_files/extraction_script/extraction.py
+++ etl_files/extraction_script/extraction.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+# Finance statements endpoints list
 FUNCTIONS = ['BALANCE_SHEET', 'CASH_FLOW', 'EARNINGS', 'INCOME_STATEMENT', 'TIME_SERIES_DAILY']
 
+# For loop through each finance statement within the list to extract API data and save it as json file
 for item in FUNCTIONS:
     try:
         url = f'https://www.alphavantage.co/query?function={item}&symbol=GOOG&apikey=E1CXMWH297VO9QOF' # type: ignore
@@ -11,5 +13,5 @@ for item in FUNCTIONS:
     except:
         print('Error: ', response.status_code)
 
-    with open(f'/home/revson/Projects/Invest_Wise/project_directory/json_files/{item}.json', 'x') as json_file:
+    with open(f'./json_files/{item}.json', 'x') as json_file:
         json.dump(data, json_file, indent=4)
